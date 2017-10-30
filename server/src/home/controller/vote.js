310c2275066f985c5f98ced5aa62bc5fa7163e9e
zhentaoo <1904974327@qq.com>
2017-10-30 21:31:04 +0800

    接口

--- server/src/home/controller/vote.js
+++ server/src/home/controller/vote.js
@@ -62,17 +62,15 @@ export default class extends think.controller.rest {
 
     // 投票，id，userkey，vote
     /**
-    fetch('http://10.12.59.81:8360/home/vote/59f1e6a85b0a94884a8ca193', {
+    fetch(`http://10.12.59.162:8360/home/vote/59f2e301c65f4a846868e8e7`, {
       method: 'PUT',
       headers: {"Content-Type": "application/x-www-form-urlencoded"},
-      body: "VoteResult="+JSON.stringify({vote: '海贼', userkey: '我的天xxx'})
+      body: "VoteResult="+JSON.stringify({
+        userKey: 'aaa123',
+        userName: 'aaa',
+        vote: '日本'})
     })
 
-    fetch('http://10.12.59.81:8360/home/vote/59f1e6a85b0a94884a8ca193', {
-      method: 'PUT',
-      headers: {"Content-Type": "application/x-www-form-urlencoded"},
-      body: "VoteResult="+JSON.stringify({vote: '火影', userkey: '阿斯顿xxx'})
-    })
     **/
     async putAction(){
       if (!this.id) {
@@ -80,15 +78,26 @@ export default class extends think.controller.rest {
       }
 
       let pk = await this.modelInstance.getPk();
-      let originData = await this.modelInstance.where({[pk]: this.id}).find().VoteResult || {};
-      console.log('originData:', originData);
+      console.log('**pk:**', pk);
 
-      console.log('this.post:', this.post());
+      let data = await this.modelInstance.where({[pk]: this.id}).find()
+      console.log('**data:**', data);
+
+      let originData = data.VoteResult;
+      if (!originData) {
+        originData = [];
+      }
+      console.log('**originData:**', originData);
+
+      console.log('**this.post:**', this.post());
       let newData = JSON.parse(this.post().VoteResult);
-      let {userkey, vote} = newData;
-      console.log('newData:', newData);
+      let {userKey, vote, userName} = newData;
+      console.log('**newData:**', newData);
 
-      let rows = await this.modelInstance.where({[pk]: this.id}).update({VoteResult: newData});
+      originData.push(newData)
+      console.log('**originData**', originData);
+
+      let rows = await this.modelInstance.where({[pk]: this.id}).update({VoteResult: originData});
       return this.success({affectedRows: rows});
     }
 
