65cec008181b4eee3a2e4074b0ac96d42600cd63
zhentaoo <1904974327@qq.com>
2017-10-31 21:53:01 +0800

    all api done

--- server/src/home/controller/stat.js
+++ server/src/home/controller/stat.js
@@ -4,7 +4,7 @@ import Base from './base.js';
 
 export default class extends Base {
     /**
-    http://10.12.59.162:8360/home/index?id=59f2e301c65f4a846868e8e7
+    http://10.12.59.162:8360/home/stat?id=59f2e301c65f4a846868e8e7
    */
     async indexAction() {
       let get = await this.get();
@@ -16,8 +16,8 @@ export default class extends Base {
       let choiceList = vote.VoteInfo.choiceList;
       let voteResult = vote.VoteResult;
 
-      console.log(choiceList);
-      console.log(voteResult);
+      console.log("**choiceList**", choiceList);
+      console.log("**voteResult**", voteResult);
 
       let res = choiceList.map(el => {
         return {
@@ -25,8 +25,24 @@ export default class extends Base {
           num: 0
         }
       })
+      console.log('**res**', res);
 
-      return this.success(res);
+      res.forEach(el => {
+        let {name, num} = el;
+        console.log(name, num);
+
+        voteResult.forEach(vote => {
+          if (name == vote.vote) {
+            num++
+          }
+        })
+        el.num = num
+      })
+
+      return this.success({
+        list: res,
+        total: voteResult.length
+      });
     }
 
     /**
